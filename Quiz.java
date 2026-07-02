42facfb3c0dc66919bd8cedc06c4602f71412bab
pinnamreddyswathi <pinnamreddyswathi@gmail.com>
2026-07-02 16:39:25 +0530

    Delete Quiz.java

--- Quiz.java
+++
@@ -1,7 +0,0 @@
-public class Quiz {
-    public static void main(String[] args) {
-        System.out.println("Online Quiz Management System");
-        System.out.println("Frontend: HTML, CSS, JavaScript");
-        System.out.println("Backend Logic: Java");
-    }
-}
