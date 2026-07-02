c27d352c48341e44031e08f72482e5c404fdd88b
pinnamreddyswathi <pinnamreddyswathi@gmail.com>
2026-07-02 16:26:35 +0530

    Create Quiz class for online quiz management system

---
+++ Quiz.java
@@ -0,0 +1,7 @@
+public class Quiz {
+    public static void main(String[] args) {
+        System.out.println("Online Quiz Management System");
+        System.out.println("Frontend: HTML, CSS, JavaScript");
+        System.out.println("Backend Logic: Java");
+    }
+}
